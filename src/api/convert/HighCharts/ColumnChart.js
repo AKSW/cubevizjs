e5adb084dcd38579a98ba75e2eb183de3a0133e4
Georges Alkhouri <georges.alkhouri@gmail.com>
2016-09-23 13:41:21 +0200

    [FIX] Fixed wrong visualisation of dimEls in column chart [REBUILD] CubeViz

--- src/api/convert/HighCharts/ColumnChart.js
+++ src/api/convert/HighCharts/ColumnChart.js
@@ -16,30 +16,35 @@ export default class ColumnChart extends Chart {
         const defaultMeasure = this.selectedComponents.get('measureComponent');
 
         const dimEls = slice.getDimensionElements(dim);
-        const title = this.createTitle(List([dim]).concat(remainder));
+
+        const titleDims = (remainder) ? List([dim, remainder]) : List([dim]);
+        const title = this.createTitle(titleDims);
+
+        const remainderTitle = (remainder)
+        ? DataCube.getValue(DataCube.getLabel(slice.getDimensionElements(remainder).first()))
+        : '';
 
         const config = this.createDefaultConfig('column', title);
 
-        config.xAxis = {
-            categories: dimEls.map(dimEl => DataCube.getValue(DataCube.getLabel(dimEl))).toJS()
-        };
-
         config.yAxis = this.createYAxis();
+        config.tooltip = {
+            pointFormat: remainderTitle
+        };
+        config.series = dimEls.map(dimEl => {
+            const name = DataCube.getValue(DataCube.getLabel(dimEl));
 
-        config.series = [
-            {
-                data: slice.observations.map(o => {
+            const data = slice.getObservationsContainingDimEls(List([dimEl]))
+                .map(o => {
                     const m = slice.getMeasureElementsFromObservation(o, List([defaultMeasure])).first();
                     const val = this.toNumber(DataCube.getValue(m));
                     return val;
-                }).toJS()
-            }
-        ];
+                }).toJS();
+            return {name, data};
+        }).toJS();
 
         return (<ReactHighcharts config={config}/>);
     }
 
-    //FIXME wrong sed = 1 med = 0 representation
     render() {
         const sed = this.cubeVizResult.get('singleElementDimensions');
         const med = this.cubeVizResult.get('multiElementDimensions');
@@ -47,13 +52,13 @@ export default class ColumnChart extends Chart {
             throw new Error('Basic column chart not appropriate for more than 2 dimensions');
         if (sed.size === 1 && med.size === 0) {
             const dim = sed.get(0);
-            return this.createColumnChart(dim, List(), this.slice, this.dc);
+            return this.createColumnChart(dim, null, this.slice, this.dc);
         } else if (sed.size === 0 && med.size === 1) {
             const dim = med.get(0);
-            return this.createColumnChart(dim, List(), this.slice, this.dc);
+            return this.createColumnChart(dim, null, this.slice, this.dc);
         } else if (sed.size === 1 && med.size === 1) {
             const dim = med.get(0);
-            const remainder = List([sed.get(0)]);
+            const remainder = sed.get(0);
             return this.createColumnChart(dim, remainder, this.slice, this.dc);
         }
         throw new Error();
